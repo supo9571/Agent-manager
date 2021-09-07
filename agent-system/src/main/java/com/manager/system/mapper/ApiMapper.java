762d5215e336ea7b77e944a008f0351f5fd4a1bb
marvin <marvin@gmail.com>
2021-09-07 21:15:19 +0800

    添加 充值配置信息接口

--- agent-system/src/main/java/com/manager/system/mapper/ApiMapper.java
+++ agent-system/src/main/java/com/manager/system/mapper/ApiMapper.java
@@ -1,8 +1,10 @@
 package com.manager.system.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -15,4 +17,17 @@ public interface ApiMapper {
             "open_type jump_type,live_addr jump_url,wechat wx,qq  " +
             "from config_month_recharge where status = '1' order by update_time limit 0,1 ")
     Map getMonthConfig();
+
+    @Select("select id,cname,pay_channel,pay_type,recharge_give recharge_to_give,sort sort_id,pay_channel pay_name from config_pay where status = '1'")
+    List<Map> selectConfigPay();
+
+    @Select("SELECT open_type,recharge_other_type,recharge_type,IF(open_type = '1',live_addr,url)url,img_type vip_img,vip_name,QQ,wechat FROM config_vip_recharge " +
+            " WHERE pay_id = #{id} AND status = '1'")
+    List<Map> selectVipconfig(@Param("id") Integer id);
+
+    @Select("SELECT open_type jump_type,btn,live_addr url,bank_value FROM config_bank_recharge where pay_id = #{id} AND status = '1'")
+    List<Map> selectBankconfig(@Param("id") Integer id);
+
+    @Select("SELECT btn,diy_max,diy_min,is_diy,msg,other_name,online_config_id,pay_type pay_channel,pay_channel_code FROM config_online_recharge where pay_id = #{id} AND status = '1' ")
+    List<Map> selectOnlineconfig(@Param("id") Integer id);
 }
--- agent-system/src/main/java/com/manager/system/service/impl/ApiServiceImpl.java
+++ agent-system/src/main/java/com/manager/system/service/impl/ApiServiceImpl.java
@@ -1,12 +1,17 @@
 package com.manager.system.service.impl;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.manager.system.mapper.ApiMapper;
 import com.manager.system.service.ApiService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 /**
  * @author marvin 2021/9/7
@@ -26,7 +31,53 @@ public class ApiServiceImpl implements ApiService {
 
     @Override
     public JSONObject getBookConfig(String data) {
-        JSONObject jsonObject = JSONObject.parseObject(data);
-        return null;
+        JSONObject param = JSONObject.parseObject(data);
+        List<Map> list = apiMapper.selectConfigPay();
+        JSONArray jsonArray = new JSONArray();
+        JSONObject result = new JSONObject();
+        for (int i = 0; i < list.size(); i++) {
+            JSONObject payInfo = new JSONObject(list.get(i));
+            Integer payType = payInfo.getInteger("pay_type");
+            if(payType==1){//vip充值
+                List<Map> payList = apiMapper.selectVipconfig(payInfo.getInteger("id"));
+                payInfo.put("pay_list",payList);
+            }else if(payType==3){//银行卡充值
+                List<Map> payList = apiMapper.selectBankconfig(payInfo.getInteger("id"));
+                payInfo.put("pay_list",getBankInfo(payList));
+            }else{//线上充值
+                List<Map> payList = apiMapper.selectOnlineconfig(payInfo.getInteger("id"));
+                payInfo.put("pay_list",payList);
+            }
+            payInfo.remove("pay_type");
+            jsonArray.add(payInfo);
+        }
+        result.put("onRspPayList",jsonArray);
+        return result;
+    }
+
+    private List<JSONObject> getBankInfo(List<Map> list){
+        List<JSONObject> result = new ArrayList<>();
+        list.forEach(m->{
+            JSONObject jsonObject = new JSONObject(m);
+            if(2==jsonObject.getInteger("jump_type")){
+                jsonObject.remove("url");
+                String bankValue = "["+jsonObject.getString("bank_value")+"]";
+                JSONArray jsonArray = JSONArray.parseArray(bankValue);
+                int i =new Random().nextInt(100)+1;
+                for (int l = 0; l < jsonArray.size(); l++) {
+                    JSONObject json = jsonArray.getJSONObject(l);
+                    if(Integer.valueOf(json.getInteger("bank_rate"))>=i){
+                        jsonObject.put("bank_name",json.get("bank_name"));
+                        jsonObject.put("bank_card_num",json.get("bank_card_num"));
+                        jsonObject.put("bank_user_name",json.get("bank_user_name"));
+                        break;
+                    }
+                    i = i-Integer.valueOf(json.getInteger("bank_rate"));
+                }
+            }
+            jsonObject.remove("bank_value");
+            result.add(jsonObject);
+        });
+        return result;
     }
 }
