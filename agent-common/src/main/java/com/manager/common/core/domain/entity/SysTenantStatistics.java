25953460ed465aac0bc1cc6fe3fc4a3eed1276d1
jason <jason@gmail.com>
2021-11-04 17:00:03 +0800

    fix bug exprot

--- agent-common/src/main/java/com/manager/common/core/domain/entity/SysTenantStatistics.java
+++ agent-common/src/main/java/com/manager/common/core/domain/entity/SysTenantStatistics.java
@@ -1,5 +1,6 @@
 package com.manager.common.core.domain.entity;
 
+import com.manager.common.annotation.Excel;
 import com.manager.common.core.domain.BaseEntity;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
@@ -18,36 +19,47 @@ public class SysTenantStatistics extends BaseEntity {
     private Integer flag;
 
     @ApiModelProperty("总代/渠道号")
+    @Excel(name = "渠道号")
     private String tId;
 
-    @ApiModelProperty("总充提差额")
-    private BigDecimal totalAmount;
-
     @ApiModelProperty("总充值人数")
+    @Excel(name = "总充值人数")
     private int rechargeNum;
 
+    @ApiModelProperty("总充提差额")
+    @Excel(name = "总充提差额")
+    private BigDecimal totalAmount;
+
     @ApiModelProperty("总充值金额")
+    @Excel(name = "总充值金额")
     private BigDecimal rechargeAmount;
 
     @ApiModelProperty("总提现人数")
+    @Excel(name = "总提现人数")
     private int withdrawNum;
 
     @ApiModelProperty("总提现金额")
+    @Excel(name = "总提现金额")
     private BigDecimal withdrawAmount;
 
     @ApiModelProperty("线上赠送金额")
+    @Excel(name = "线上赠送金额")
     private BigDecimal upAwardAmount;
 
     @ApiModelProperty("线下赠送金额")
+    @Excel(name = "线下赠送金额")
     private BigDecimal lowerAwardAmount;
 
     @ApiModelProperty("直属返佣")
+    @Excel(name = "直属返佣")
     private BigDecimal underCommission;
 
     @ApiModelProperty("代理返佣")
+    @Excel(name = "代理返佣")
     private BigDecimal agentCommission;
 
     @ApiModelProperty("总安装量")
+    @Excel(name = "总安装量")
     private int registerNum;
 
 }
