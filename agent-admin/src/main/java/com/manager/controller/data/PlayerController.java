07d03c74682f6c84b3fb8c5fc21e558b062cdd91
jason <jason@gmail.com>
2021-11-12 18:47:39 +0800

    fix bug

--- agent-admin/src/main/java/com/manager/controller/data/PlayerController.java
+++ agent-admin/src/main/java/com/manager/controller/data/PlayerController.java
@@ -1,22 +1,32 @@
 package com.manager.controller.data;
 
 import com.manager.common.annotation.Log;
+import com.manager.common.config.ManagerConfig;
 import com.manager.common.core.controller.BaseController;
 import com.manager.common.core.domain.AjaxResult;
 import com.manager.common.core.domain.model.PlayUser;
 import com.manager.common.core.domain.model.PlayWater;
 import com.manager.common.core.domain.model.UserExchange;
 import com.manager.common.core.domain.model.UserLock;
+import com.manager.common.core.domain.model.param.DataAnalysisParam;
+import com.manager.common.core.domain.model.vo.DataWaterTopVO;
 import com.manager.common.enums.BusinessType;
 import com.manager.common.utils.SecurityUtils;
+import com.manager.common.utils.file.FileUtils;
+import com.manager.common.utils.poi.ExcelUtil;
 import com.manager.openFegin.DataService;
 import com.manager.system.service.BankService;
 import com.manager.system.service.PlayerService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
 
 /**
  * @author marvin 2021/8/19
@@ -31,6 +41,7 @@ public class PlayerController extends BaseController {
 
     @Autowired
     private PlayerService playerService;
+
     /**
      * 获取用户列表
      */
@@ -41,14 +52,28 @@ public class PlayerController extends BaseController {
         return dataService.getPlayers(playUser);
     }
 
+    @PreAuthorize("@ss.hasPermi('data:player:list')")
+    @ApiOperation(value = "查询玩家导出")
+    @Log(title = "查询玩家导出", businessType = BusinessType.EXPORT)
+    @PostMapping("/export")
+    public void dataWaterTopExport(@RequestBody PlayUser playUser, HttpServletResponse response) throws IOException {
+        AjaxResult ajaxResult = dataService.getPlayers(playUser);
+        ExcelUtil<PlayUser> util = new ExcelUtil(PlayUser.class);
+        String fileName = "查询玩家导出";
+        response.setContentType(MediaType.APPLICATION_OCTET_STREAM_VALUE);
+        FileUtils.setAttachmentResponseHeader(response, fileName + ".xlsx");
+        util.downloadExcel((List) ajaxResult.get("data"), fileName, response.getOutputStream());
+    }
+
+
     /**
      * 查询玩家余额
      */
     @PreAuthorize("@ss.hasPermi('data:player:curr')")
     @ApiOperation(value = "查询玩家余额")
     @GetMapping("/curr")
-    public AjaxResult curr(Long uid,String beginTime,String endTime) {
-        return dataService.getPlayerCurr(uid,beginTime,endTime);
+    public AjaxResult curr(Long uid, String beginTime, String endTime) {
+        return dataService.getPlayerCurr(uid, beginTime, endTime);
     }
 
     /**
@@ -121,7 +146,7 @@ public class PlayerController extends BaseController {
     @PostMapping("/popularize")
     public AjaxResult popularize(String uid) {
         startPage();
-        return AjaxResult.success("查询成功",getDataTable(playerService.getPopularizes(uid)));
+        return AjaxResult.success("查询成功", getDataTable(playerService.getPopularizes(uid)));
     }
 
     /**
